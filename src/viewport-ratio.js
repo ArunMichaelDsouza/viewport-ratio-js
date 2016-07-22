da0ad72b54e75835fc9191c308053ba3643dd43e
Arun Michael Dsouza <amdsouza92@gmail.com>
2016-07-23 02:20:17 +0530

    Target element detection + percent calculation logic added

--- src/viewport-ratio.js
+++ src/viewport-ratio.js
@@ -1,30 +1,45 @@
+//trace - all elements
+//round - total percent
+//scroll - enable scroll
+//init - initialise
 ;(function() {
+	'use strict';
 
-	"use strict";
-
-	function Viewport(height, width) {
-		this.height = height;
+	// Constructor function to create viewport instance
+	function Viewport(width, height) {
 		this.width = width;
-		this.adClass = 'g_ad';
+		this.height = height;
+		this.targetElements = [];
+		this.viewportArea = this.width + this.height;
+		this.targetElementsArea = 0;
 	}
 
-	function getTag(el) {
-		return document.getElementsByTagName(el)[0];
-	}
-
-	var clientHeight = getTag('html').clientHeight,
-		clientWidth = getTag('html').clientWidth;
-
-	var viewport = new Viewport(clientHeight, clientWidth);
-
-	Viewport.prototype.getAllAds = function() {
-		var ads = document.getElementsByClassName(this.adClass);
-
-		for(var i = 0; i < ads.length; i ++) {
-			console.log(ads[i].clientHeight);
-			console.log(ads[i].clientWidth);
+	// Function to get target element's area (in px)
+	Viewport.prototype.getTargetElementsArea = function(elements) {
+		var elementList = document.querySelectorAll(elements);
+		for(var i = 0; i < elementList.length; i ++) {
+			this.targetElements.push({
+				name: elementList[i].nodeName,
+				width: elementList[i].clientWidth,
+				height: elementList[i].clientHeight 
+			});
+			this.targetElementsArea += this.targetElements[i].width;
+			this.targetElementsArea += this.targetElements[i].height;
 		}
+		return this.targetElementsArea;		
 	};
 
-	viewport.getAllAds();
+	Viewport.prototype.calculateTargetAreaPercent = function() {
+		return ( this.targetElementsArea / this.viewportArea) * 100;
+	};
+
+	// Get viewport dimensions
+	var viewportWidth = window.innerWidth,
+		viewportHeight = window.innerHeight;
+
+	// Create viewport instance
+	var viewport = new Viewport(viewportWidth, viewportHeight);
+
+	viewport.getTargetElementsArea('.div_ratio');
+	console.log(viewport.calculateTargetAreaPercent());
 })();
